f1eeab9fc8f313e21c650cdbb9fb9eb4076562db
Daniel <danielrs9504@gmail.com>
2021-04-10 13:35:11 -0500

    Agrega el estado basico de la aplicacion

---
+++ src/store.js
@@ -0,0 +1,20 @@
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+
+const reducer = combineReducers({});
+
+const initialState = {
+  player1: { name: "", score: "" },
+  player2: { name: "", score: "" },
+};
+
+const middleware = [thunk];
+
+const store = createStore(
+  reducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
+
+export default store;
